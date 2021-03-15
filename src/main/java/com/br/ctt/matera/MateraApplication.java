5ec0d42629c9dd1ea44c076e3f51e3a26c25af7d
cyntiagoulart <cyntiartg@gmail.com>
2021-03-14 21:09:18 -0300

    Criação de projeto Maven com Spring Web Server.

---
+++ src/main/java/com/br/ctt/matera/MateraApplication.java
@@ -0,0 +1,13 @@
+package com.br.ctt.matera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/ctt/matera/MateraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ctt.matera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
